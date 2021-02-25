f1e92891109ebceeb01d35d0424ff8c3d8f33c5b
TryHarder-Python <gleb.white90@gmail.com>
2021-02-25 04:22:55 +0200

    Upgrate forms

--- blog/urls.py
+++ blog/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', views.post_list, name='post_list'),
     path('posts/<int:pk>/', views.post_detail, name='post_detail'),
     path('posts/new/', views.post_new, name='post_new'),
+    path('posts/<int:pk>/edit/', views.post_edit, name='post_edit')
 ]
